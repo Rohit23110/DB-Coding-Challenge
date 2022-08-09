b23ab5306dab565eae7f4cac4e0149204ecc9d44
Dhanu-k <dhanshreekulkarni030@gmail.com>
2022-08-09 16:35:40 +0530

    Added basic authentication

--- java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
@@ -8,4 +8,6 @@ import com.db.grad.javaapi.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+	Boolean existsByEmail(String email);
+	User findByEmail(String email);
 }
